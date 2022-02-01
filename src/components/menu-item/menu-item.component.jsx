d70340d1b4f19230bed0b0665292fb99ea26f1a3
unknown <ansulaoari88@gmail.com>
2022-02-01 21:45:19 +0530

    update our menu-items to navigate withRouter higher oreder component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
-const MenuItem = ({title,imageUrl,size}) => (
+const MenuItem = ({title,imageUrl,size,history,linkUrl,match}) => (
+<div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
   <div className={`${size} menu-item`} >
    <div className='background-image'
     style={{backgroundImage: `url(${imageUrl})`}}/>
@@ -11,6 +13,7 @@ const MenuItem = ({title,imageUrl,size}) => (
 
        </div>  
      </div>
+</div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
